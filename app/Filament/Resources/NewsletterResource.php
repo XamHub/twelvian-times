06102376422c632d4b9998b26d2d551ec077aa96
Makzie <maxklumper@outlook.com>
2024-11-24 17:02:26 +0100

    feat: Newsletters

---
+++ app/Filament/Resources/NewsletterResource.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\NewsletterResource\Pages;
+use App\Filament\Resources\NewsletterResource\RelationManagers;
+use App\Models\Newsletter;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Forms\Set;
+use Filament\Forms\Get;
+use Illuminate\Support\Str;
+
+class NewsletterResource extends Resource
+{
+    protected static ?string $model = Newsletter::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('subject')
+                    ->reactive()
+                    ->required()
+                    ->live(onBlur: true)
+                    ->afterStateUpdated(function (Get $get, Set $set, ?string $old, ?string $state) {
+                        if (($get('slug') ?? '') !== Str::slug($old)) {
+                            return;
+                        }
+                        $set('slug', Str::slug($state));
+                    })
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('slug')
+                    ->required()
+                    ->maxLength(255)
+                    ->unique(ignoreRecord: true)
+                    ->readonly(),
+                Forms\Components\RichEditor::make('body')
+                    ->required(),
+                Forms\Components\TextInput::make('author')
+                    ->nullable(),
+                Forms\Components\DateTimePicker::make('published_at')
+                    ->nullable()
+                    ->seconds(false)
+                    ->label('Publish Date'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('subject')->sortable()->searchable(),
+                Tables\Columns\TextColumn::make('author')->sortable()->searchable(),
+                Tables\Columns\TextColumn::make('published_at')->dateTime()->label('Publish Date'),
+                Tables\Columns\TextColumn::make('created_at')->dateTime(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListNewsletters::route('/'),
+            'create' => Pages\CreateNewsletter::route('/create'),
+            'edit' => Pages\EditNewsletter::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Models/Newsletter.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Newsletter extends Model
+{
+    protected $fillable = [
+        'subject',
+        'slug',
+        'body',
+        'author',
+        'published_at',
+    ];
+
+    protected static function booted()
+    {
+        static::saving(function ($newsletter) {
+            $newsletter->slug = Str::slug($newsletter->subject);
+        });
+    }
+}
